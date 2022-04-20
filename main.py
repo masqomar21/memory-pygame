05642676719f0819d76f0d0f6db62c140c2a5291
masQ-21dev <74146018+masQ-21dev@users.noreply.github.com>
2022-04-20 20:15:18 +0700

    day 1

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+from email.header import Header
 from pygame import *
 from src.screen import *
 
 init()
+WIDTH = 800
+HEIGHT = 600
 
-screen_init(800, 600)
+White = (255, 255, 255)
+black = (0, 0, 0)
+
+fps = 60
+timer = time.Clock()
+# Initialize the screen
+screen = display.set_mode((WIDTH, HEIGHT))
+display.set_caption("remaider game")
+
+running = True
+while running :
+    timer.tick(fps)
+    screen.fill(White)
+
+    for even in event.get():
+        if even.type == QUIT:
+            running = False
+
+
+    display.flip()
+
 
 quit()
 
